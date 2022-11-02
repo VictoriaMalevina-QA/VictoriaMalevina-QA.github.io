1e10db4982f59091afb9cf772941b79b00d86650
Victoria Malevina <117194434+VictoriaMalevina-QA@users.noreply.github.com>
2022-11-02 22:28:47 +0500

    Update js.js

--- js.js
+++ js.js
@@ -1,8 +1,12 @@
+const random = (min, max) => {
+    const rand = min + Math.random() * (max - min + 1);
+    return Math.floor(rand);
+}
+
 const button = document.getElementById('btn');
 button.addEventListener('mouseover', function () {
     button.style.left = `${Math.ceil(Math.random() * 60)}%`;
     button.style.top = `${Math.ceil(Math.random() * 60)}%`;
-    var range = 20;
 });
 button.addEventListener('click', function () {
     alert('Virtual hugs! ٩(｡•́‿•̀｡)۶')
